4875ae7dbd80a0d2c91963c92f353b2822f21227
BryanBmmF <bryan.bmmf@gmail.com>
2021-03-16 19:15:17 -0600

    Primer Pull request

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(DemoApplication.class, args);
+
+        System.out.println("Primer pull request");
     }
+
 
 }
